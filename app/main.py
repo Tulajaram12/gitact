d4ad5196d300a5392cf0b8a5201fc81715a328ae
Tulajaram12 <96332761+Tulajaram12@users.noreply.github.com>
2026-05-26 17:40:52 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello from DevOps App 🚀"})
+    return jsonify({"message": "Hello man from DevOps App 🚀"})
 
 @app.route("/health")
 def health():
